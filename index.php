62dda473200eac54cdc9dbdf42de1272213fba20
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-11-21 22:56:49 +0800

    index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 include('Net/SSH2.php');
 
-$ssh = new Net_SSH2('localhost');
-$ssh->login('root', 'password');
+$ssh = new Net_SSH2('localhost');   // 登入伺服器
+$ssh->login('root', 'password');    // 輸入登入的帳號與密碼
 
 echo "<pre>" . $ssh->read('[prompt]') . "</pre>"; // 登入訊息
 
